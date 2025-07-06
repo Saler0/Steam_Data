d0c069c626bb0308db0ba67ca87ffcd99a5db1f9
Saler0 <milenko.espinozag@gmail.com>
2025-07-06 14:25:39 +0200

    Deleted: se borra un archivo que no tiene uso

--- check_spark.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-import os
-
-print("JAVA_HOME =", os.environ.get("JAVA_HOME"))
-spark = SparkSession.builder.appName("TestJava").getOrCreate()
-print("Spark initialized")
-spark.stop()
