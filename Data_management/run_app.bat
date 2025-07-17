9dd337c38260032ac7aec5802ce847c2df7bb0c2
Saler0 <milenko.espinozag@gmail.com>
2025-07-17 21:00:25 +0200

    Fix: se hace una limpieza de los contenedores que podrian haber antes de volver a ejecutar el .bat

--- Data_management/run_app.bat
+++ Data_management/run_app.bat
@@ -5,6 +5,28 @@ REM -------------------------------------------------
 
 cd /d %~dp0
 
+
+
+echo.
+echo ============================
+echo Parando cualquier contenedor existente...
+echo ============================
+docker stop mongo steam_data-app 2>nul
+
+echo.
+echo ============================
+echo Eliminando contenedores detenidos...
+echo ============================
+docker rm mongo steam_data-app 2>nul
+
+
+echo.
+echo ============================
+echo Deteniendo y eliminando contenedores del compose…
+echo ============================
+docker compose down
+
+
 echo.
 echo ============================
 echo Reconstruyendo y levantando...
