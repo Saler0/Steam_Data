003f54250581b1bbcddeae8b75e40fda65741c7a
Saler0 <milenko.espinozag@gmail.com>
2025-09-07 15:00:08 +0200

    Fix: se añaden mas limpieza a los textos para limpiar remanentes de etiquetas de HTML o BBCode

--- Data_management/landing_to_trusted/funciones_trusted.py
+++ Data_management/landing_to_trusted/funciones_trusted.py
@@ -39,6 +39,11 @@ def clean_text(text):
     text = re.sub(r'\s*\n\s*', '\n', text)       # limpiar bordes de líneas
     text = re.sub(r'\n{3,}', '\n\n', text)       # evitar 3+ saltos seguidos
     text.replace('<img src="','')
+    text.replace('\\t','')
+    text.replace('\t','')
+    text.replace('"<a href="','')
+    text.replace('rel="bookmark"','')
+    text.replace('title="','')
     return text.strip()
 
 clean_text_udf = udf(clean_text, StringType())
