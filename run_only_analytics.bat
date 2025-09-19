464b3fac9698bd5f1274f59e3bbd5bf0c6f57619
Saler0 <milenko.espinozag@gmail.com>
2025-09-19 20:29:50 +0200

    Chore: tratando de hacer funcionar el run only analytics

--- run_only_analytics.bat
+++ run_only_analytics.bat
@@ -17,18 +17,38 @@ docker rm -f mongo 2>nul
 docker rm -f steam_mlflow 2>nul
 docker rm -f steam_analytics 2>nul
 docker rm -f data_management_pipeline 2>nul
+docker rm -f postgres_db 2>nul
 
 echo.
 echo ============================
 echo Levantando solo Mongo + MLflow y Analytics...
 echo ============================
-docker compose up -d mongo mlflow analytics
+docker compose up -d mongo postgres mlflow analytics
+
+
+
+REM Ejecuta el comando DVC DENTRO del contenedor 'analytics'
+docker compose exec analytics dvc repro clustering
+if errorlevel 1 (
+    echo.
+    echo ERROR: Fallo al ejecutar el pipeline de DVC.
+    echo Revisa los logs del contenedor para ver el error.
+    pause
+    exit /b 1
+)
 
 echo.
-echo ============================
-echo Ver logs de analytics (Ctrl+C para salir)...
-echo ============================
-docker compose logs -f analytics
+echo =======================================================
+echo [PASO 4 de 4] Pipeline finalizado con exito!
+echo =======================================================
+echo.
+echo Los resultados del clustering se encuentran en:
+echo - El fichero: data/processed/clusters.parquet
+echo - La base de datos MongoDB (coleccion: game_clusters)
+echo.
+echo Puedes apagar los contenedores ejecutando: docker-compose down
+echo.
+pause
 
 endlocal
 pause
