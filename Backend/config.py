f804da4c812d30f4f033814a0e017aa08d2a9752
Saler0 <milenko.espinozag@gmail.com>
2025-10-07 18:26:50 +0200

    Delete: se elimina las configuraciones erroneas

--- Backend/config.py
+++ Backend/config.py
@@ -5,14 +5,3 @@ DECISION_RULES = {
     'price_margin_pct': 0.10,
     'price_normal_margin_pct': 0.10,
 }
-
-# Mongo collections
-COLLECTIONS = {
-    'client_games': 'juegos_clientes',
-    'steam_games': 'juegos_steam',
-}
-
-# Fields used by decision-rule pipelines
-FIELDS = {
-    'steam_price': 'price',
-}
